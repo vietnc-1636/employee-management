b504fc8fa4a9fea9fe5b7512e25a8e81b2805e90
vietnc-1636 <nguyen.cong.viet@sun-asterisk.com>
2025-10-28 15:19:41 +0700

    task: init admin lte

---
+++ src/main/java/com/example/employee_management/controller/DashboardController.java
@@ -0,0 +1,13 @@
+package com.example.employee_management.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/")
+    public String showDashboard() {
+        return "index";
+    }
+}
